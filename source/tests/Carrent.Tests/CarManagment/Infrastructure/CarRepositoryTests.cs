88cd18e1ec91c98ebf5a269fb23cf6cf8460c8f0
marctraber <traber.marc@hispeed.ch>
2021-08-15 22:29:19 +0200

    Add first Part of Unit Tests Car

---
+++ source/tests/Carrent.Tests/CarManagment/Infrastructure/CarRepositoryTests.cs
@@ -0,0 +1,93 @@
+﻿using Carrent.CarManagment.Domain;
+using Carrent.CarManagment.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Carrent.Tests.CarManagment.Infrastructure
+{
+    public class CarRepositoryTests : IClassFixture<SharedDatabaseFixture>
+    {
+        public CarRepositoryTests(SharedDatabaseFixture fixture) => Fixture = fixture;
+
+        public SharedDatabaseFixture Fixture { get; }
+
+        [Fact]
+        public void Can_add_Car()
+        {
+            using (var transaction = Fixture.Connection.BeginTransaction())
+            {
+                using (var context = Fixture.CreateContext(transaction))
+                {
+                    // arrange
+                    var carClassController = new CarClassRepository(context);
+                    var carController = new CarRepository(context);
+
+                    var carCalss = carClassController.GetAll().First();
+                    var newCar = new Car()
+                    {
+                        Id = Guid.NewGuid(),
+                        Class = carCalss,
+                        ClassId = carCalss.Id,
+                        Brand = "VW Tiguan",
+                        Type = "SUV"
+                    };
+
+                    // act
+                    carController.Insert(newCar);
+
+                    // assert
+                    var dbCar = carController.GetById(newCar.Id);
+                    Assert.Equal(newCar, dbCar);
+                }
+            }
+        }
+
+        [Fact]
+        public void Can_removeById_Car()
+        {
+            using (var transaction = Fixture.Connection.BeginTransaction())
+            {
+                using (var context = Fixture.CreateContext(transaction))
+                {
+                    // arrange
+                    var carController = new CarRepository(context);
+
+                    var existsCar = carController.GetAll().First();
+
+                    // act
+                    carController.Remove(existsCar.Id);
+
+                    // assert
+                    var dbCar = carController.GetById(existsCar.Id);
+                    Assert.Null(dbCar);
+                }
+            }
+        }
+
+        [Fact]
+        public void Can_removeByObject_Car()
+        {
+            using (var transaction = Fixture.Connection.BeginTransaction())
+            {
+                using (var context = Fixture.CreateContext(transaction))
+                {
+                    // arrange
+                    var carController = new CarRepository(context);
+
+                    var existsCar = carController.GetAll().First();
+
+                    // act
+                    carController.Remove(existsCar);
+
+                    // assert
+                    var dbCar = carController.GetById(existsCar.Id);
+                    Assert.Null(dbCar);
+                }
+            }
+        }
+    }
+}
---
+++ source/tests/Carrent.Tests/CarManagment/Infrastructure/SharedDatabaseFixture.cs
@@ -0,0 +1,82 @@
+﻿using Carrent.CarManagment.Domain;
+using Carrent.Comman.Infrastructure.DbContext;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Common;
+
+namespace Carrent.Tests.CarManagment.Infrastructure
+{
+    public class SharedDatabaseFixture : IDisposable
+    {
+        private static readonly object _lock = new object();
+        private static bool _databaseInitialized;
+
+        public SharedDatabaseFixture()
+        {
+            Connection = new SqlConnection(@"Data Source=.; Database=CarRent_XUnit_Tests; Trusted_Connection=True");
+
+            Seed();
+
+            Connection.Open();
+        }
+
+        public DbConnection Connection { get; }
+
+        public CarRentDbContext CreateContext(DbTransaction transaction = null)
+        {
+            var context = new CarRentDbContext(new DbContextOptionsBuilder<CarRentDbContext>().UseSqlServer(Connection).Options);
+
+            if (transaction != null)
+            {
+                context.Database.UseTransaction(transaction);
+            }
+
+            return context;
+        }
+
+        private void Seed()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateContext())
+                    {
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+
+                        var carClass = new CarClass()
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = "Standard",
+                            PricePerDay = 100
+                        };
+                        context.CarClasses.Add(carClass);
+                        context.SaveChanges();
+
+                        var car = new Car()
+                        {
+                            Id = Guid.NewGuid(),
+                            ClassId = carClass.Id,
+                            Class = carClass,
+                            Brand = "Audi A3",
+                            Type = "Limousine"
+                        };
+                        context.Cars.Add(car);
+                        context.SaveChanges();
+
+
+
+
+                        context.SaveChanges();
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+        }
+
+        public void Dispose() => Connection.Dispose();
+    }
+}
